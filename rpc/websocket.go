1f7af063ccd1bf5add4ede1c5c4bed6c33d88541
carl <carl.shen@apple.com>
2026-02-06 20:15:57 +0800

    add

--- rpc/websocket.go
+++ rpc/websocket.go
@@ -34,8 +34,8 @@ import (
 )
 
 const (
-	wsReadBuffer       = 8192
-	wsWriteBuffer      = 8192
+	wsReadBuffer       = 1024
+	wsWriteBuffer      = 1024
 	wsPingInterval     = 30 * time.Second
 	wsPingWriteTimeout = 5 * time.Second
 	wsPongTimeout      = 30 * time.Second
