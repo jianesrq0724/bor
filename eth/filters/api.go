c8547324c0e584a134722fb8848055085403de7f
carl <carl.shen@apple.com>
2026-02-09 16:08:43 +0800

    add

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -189,34 +189,22 @@ func (api *FilterAPI) NewPendingTransactions(ctx context.Context, fullTx *bool) 
 		pendingTxSub := api.events.SubscribePendingTxs(txs)
 		defer pendingTxSub.Unsubscribe()
 
-		signer := types.LatestSigner(api.sys.backend.ChainConfig())
+		chainConfig := api.sys.backend.ChainConfig()
 
 		for {
 			select {
 			case txs := <-txs:
-				if fullTx != nil && *fullTx {
-					result := make([]map[string]interface{}, len(txs))
-					for i, tx := range txs {
-						from, _ := types.Sender(signer, tx)
-						result[i] = map[string]interface{}{
-							"hash":                 tx.Hash(),
-							"from":                 from,
-							"to":                   tx.To(),
-							"value":                (*hexutil.Big)(tx.Value()),
-							"input":                hexutil.Bytes(tx.Data()),
-							"type":                 hexutil.Uint64(tx.Type()),
-							"gasPrice":             (*hexutil.Big)(tx.GasPrice()),
-							"maxFeePerGas":         (*hexutil.Big)(tx.GasFeeCap()),
-							"maxPriorityFeePerGas": (*hexutil.Big)(tx.GasTipCap()),
-						}
+				// To keep the original behaviour, send a single tx hash in one notification.
+				// TODO(rjl493456442) Send a batch of tx hashes in one notification
+				latest := api.sys.backend.CurrentHeader()
+
+				for _, tx := range txs {
+					if fullTx != nil && *fullTx {
+						rpcTx := ethapi.NewRPCPendingTransaction(tx, latest, chainConfig)
+						_ = notifier.Notify(rpcSub.ID, rpcTx)
+					} else {
+						_ = notifier.Notify(rpcSub.ID, tx.Hash())
 					}
-					_ = notifier.Notify(rpcSub.ID, result)
-				} else {
-					hashes := make([]common.Hash, len(txs))
-					for i, tx := range txs {
-						hashes[i] = tx.Hash()
-					}
-					_ = notifier.Notify(rpcSub.ID, hashes)
 				}
 			case <-rpcSub.Err():
 				return
