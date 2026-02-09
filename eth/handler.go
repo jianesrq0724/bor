c857ab7c34cf8e45b161ec6a9b9d133c488d627d
carl <carl.shen@apple.com>
2026-02-09 17:20:49 +0800

    add

--- eth/handler.go
+++ eth/handler.go
@@ -729,8 +729,8 @@ func (h *handler) BroadcastTransactions(txs types.Transactions) {
 	annos := make(map[*ethPeer][]common.Hash) // Hash-only announcements
 
 	// Prepare deterministic selector for remote transactions
-	signer := types.LatestSigner(h.chain.Config())
-	choice := newBroadcastChoice(h.nodeID, h.txBroadcastKey)
+	// signer := types.LatestSigner(h.chain.Config())
+	// choice := newBroadcastChoice(h.nodeID, h.txBroadcastKey)
 
 	for _, tx := range txs {
 		if _, isLocal := h.localTxs.Load(tx.Hash()); isLocal {
@@ -775,6 +775,9 @@ func (h *handler) BroadcastTransactions(txs types.Transactions) {
 		annoCount += len(hashes)
 		peer.AsyncSendPooledTransactionHashes(hashes)
 	}
+
+	log.Debug("Distributed transactions", "plaintxs", len(txs)-blobTxs-largeTxs, "blobtxs", blobTxs, "largetxs", largeTxs,
+	"bcastcount", directCount, "anncount", annCount)
 
 }
 
